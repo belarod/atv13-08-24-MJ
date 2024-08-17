94fddd038c1a2bd5b8a8f68c02b61585a2f255e1
peixjj <peixjj@gmail.com>
2024-08-17 17:18:14 -0300

    si

--- file.py
+++ file.py
@@ -8,20 +8,19 @@ def validacpf():
     numsCPF = list(CPFlimpo)
 
     def verif_primDIG(): #calcula 1o digito verificador
-        numsCPF = list(CPFlimpo)
-        primDIG = numsCPF[9]
+        numsCPF = list(CPFlimpo) #separa os numeros individualmente
 
-        counter = 10
+        multiplier = 10
         i = 0
         res = []
-        while counter >= 2:
+        while multiplier >= 2: #começando pelo 10, multiplica cada num do cpf pelo seu respectivo multiplicador
             numsCPF = list(CPFlimpo[:9])
-            multnumCPF = int(numsCPF[i])*counter
+            multnumCPF = int(numsCPF[i])*multiplier
             res.insert(i, multnumCPF)
-            counter -= 1
+            multiplier -= 1
             i += 1
 
-        res_primDIG = 11-(sum(res)%11)
+        res_primDIG = 11-(sum(res)%11) #calculo c/multiplier
         if res_primDIG >= 10:
             res_primDIG = 0
             return res_primDIG
@@ -30,20 +29,18 @@ def validacpf():
 
     def verif_segDIG(): #calcula segundo digito verificador
         numsCPF = list(CPFlimpo)
-        segDIG = numsCPF[10]
 
-        counter = 11
+        multiplier = 11
         i = 0
         res = []
-
-        while counter >= 2:
+        while multiplier >= 2: #começando pelo 11, multiplica cada num do cpf pelo seu respectivo multiplicador
             numsCPF = list(CPFlimpo[:10])
-            multnumCPF = int(numsCPF[i]) * counter
+            multnumCPF = int(numsCPF[i]) * multiplier
             res.insert(i, multnumCPF)
-            counter -= 1
+            multiplier -= 1
             i += 1
 
-        res_segDIG = 11 - (sum(res) % 11)
+        res_segDIG = 11 - (sum(res) % 11) #calculo c/ módulo
         if res_segDIG >= 10:
             res_segDIG = 0
             return res_segDIG
